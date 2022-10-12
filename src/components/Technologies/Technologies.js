91d59bdae31f61475a50eb8448701c1a64771379
AmitNahve <amitf607@gmail.com>
2022-10-12 13:53:01 +0300

    updates

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,56 +1,66 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend,  } from 'react-icons/di';
-import{SiCsharp} from 'react-icons'
+import { DiFirebase, DiReact, DiZend, DiVisualstudio, DiDocker } from 'react-icons/di';
+import { SiCsharp } from 'react-icons'
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
-    <br/>
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>I've Learned a range of technologies in the Web development world,
       From Back-end To Design
     </SectionText>
-    <List>
+    {/* <List>
       <ListItem>
 
       </ListItem>
-    </List>
+    </List> */}
     <List>
       <ListItem>
-        <DiFirebase size="3rem"/>
-          <ListContainer>
-            <ListTitle>Back-End</ListTitle>
-            <ListParagraph>Experience with <br />
-              C# .net<br />
-              Asp .net core<br />
-              Node.js
-            </ListParagraph>
-          </ListContainer>
-        
+        <DiVisualstudio size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>Experience with: <br />
+            C# .net<br />
+            Asp .net core<br />
+            Node.js
+          </ListParagraph>
+        </ListContainer>
+
       </ListItem>
       <ListItem>
-        <DiFirebase size="3rem"/>
-          <ListContainer>
-            <ListTitle>Front-End</ListTitle>
-            <ListParagraph>Experience with <br />
-              Angular.js<br />
-              React.js<br />
-              Html,css,js
-            </ListParagraph>
-          </ListContainer>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>Experience with: <br />
+            Angular.js<br />
+            React.js<br />
+            Html,css,js
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
       <ListItem>
-        <DiFirebase size="3rem"/>
-          <ListContainer>
-            <ListTitle>Data Bases</ListTitle>
-            <ListParagraph>Experience with <br />
-              MySql<br />
-              SQlite<br />
-              MongoDB
-            </ListParagraph>
-          </ListContainer>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Data Bases</ListTitle>
+          <ListParagraph>Experience with: <br />
+            MySql<br />
+            SQlite<br />
+            MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiDocker size="3rem" />
+        <ListContainer>
+          <ListTitle>Computing Cloud</ListTitle>
+          <ListParagraph>Experience with: <br />
+            Azure<br />
+            Docker
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
     </List>
   </Section>
