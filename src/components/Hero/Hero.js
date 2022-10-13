29766e2f1f97c293705bacd6a7f5a4a162e4df33
AmitNahve <amitf607@gmail.com>
2022-10-13 11:27:55 +0300

    ui fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
       Graduated software development course at Sela College as part of the "Combat Soldiers to the High tech" program, looking for my first position as junior full-stack developer.
 Person of high ethics and morals, great people skills, ability to work as part of a team, at complex and stressful environments In addition to working on different subjects at the same time and multi-tasking. Experienced in leading, managing and driving people.
       </SectionText>
-      <Button onClick={()=> window.location = "https://api.whatsapp.com/send?phone=9720584008775&text=Hello%2C%20I'm%20here%20to%20tell%20you%20about%20your%20next%20job"}><IoLogoWhatsapp></IoLogoWhatsapp>Contact me</Button>
+      <Button onClick={()=> window.location = "https://api.whatsapp.com/send?phone=9720584008775&text=Hello%2C%20I'm%20here%20to%20tell%20you%20about%20your%20next%20job"}><IoLogoWhatsapp size="3rem" style={{}}></IoLogoWhatsapp>Contact me</Button>
     </LeftSection>
   </Section>
 
